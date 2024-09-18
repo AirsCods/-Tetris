5e7ce2582026a4facc25d0f2db36b3e0ed0954f8
airscods <airscods@gmail.com>
2024-09-18 19:04:10 +0700

    create drow func

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
 ﻿
+Console.SetWindowSize(40, 30);
+Console.SetBufferSize(40, 30);
+
+int x1 = 2;
+int y1 = 3;
+
+char c1 = '*';
+
+Drow(x1, y1, c1);
+
+Console.ReadLine();
+
+
+static void Drow(int x, int y, char c)
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(c);
+}
